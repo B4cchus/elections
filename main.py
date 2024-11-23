8e5aa2cf80aea9160540ef10e223466ed956f984
B4cchus <a.severskij@gmail.com>
2024-11-23 16:29:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from typing import Optional
-
+import os, json
 from fastapi import FastAPI
 
 app = FastAPI()
 
+sheets_key = os.environ.get('GOOGLE_JSON_KEY')
+key_data = json.loads(sheets_key)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": {"Hello World", key_data["type"]}}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
