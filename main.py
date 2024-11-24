a5e2ce7bb2aed07960cd7b194fee732233f3a0c6
B4cchus <a.severskij@gmail.com>
2024-11-24 12:40:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from typing import Optional
 import os, json, gspread, datetime
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 gc = gspread.service_account_from_dict(json.loads(os.environ.get('GOOGLE_JSON_KEY')))
 sh = gc.open("ElectionsDB")
 
 @app.get("/")
 async def root():
-    wks.update_acell('A1', str(datetime.datetime.now()))
+    sh.sheet1.update_acell('A1', str(datetime.datetime.now()))
     return {"message": datetime.datetime.now()}
 
-@app.get("/elections/{election_id}")
-async def cands(election_id: str):
-    return {"cands": sh.named_range(election_id+"_cds")}
+@app.get("/vote")
+async def vote(election_id: str = "test_election"):
+    return {"Nothing here": None}
+
+@app.get("/view")
+async def cands(election_id: str = "test_election"):
+    return {"cands": sh.named_range(election_id+"_cds"), "vrs": sh.named_range(election_id+"_vrs")}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
