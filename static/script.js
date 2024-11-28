6e46802d1fd23a0124b346203e0d70ac9cedda0e
ASeverskij <a.severskij@gmail.com>
2024-11-28 01:11:29 +0100

    Still developing...

--- static/script.js
+++ static/script.js
@@ -82,9 +82,9 @@ function removeCandidate(name) {
 }
 
 // Submit the ballot
-async function sha256(message) {
+function sha256(message) {
   const msgBuffer = new TextEncoder().encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashBuffer = crypto.subtle.digest('SHA-256', msgBuffer);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 }
